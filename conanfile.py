dff3d2b2597350714009980c8238a562ec510fba
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2022-07-14 09:28:09 +0200

    Get rid of folly for now

--- conanfile.py
+++ conanfile.py
@@ -13,14 +13,12 @@ class Recipe(ConanFile):
         self.requires("openssl/1.1.1o")
         self.requires("sol2/3.3.0")
         self.requires("sqlitecpp/3.1.1")
-        self.requires("folly/2022.01.31.00")
         self.requires("boost/1.79.0")
         self.requires("fmt/7.0.3")
         self.requires("stb/cci.20210910")
         self.requires("taocpp-pegtl/3.2.6")
         self.requires("ms-gsl/4.0.0")
         self.requires("type_safe/0.2.2")
-
 
         # Testing only dependencies below
         self.requires("catch2/3.0.1")
