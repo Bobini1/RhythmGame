2cae0aa21d41b7a52bbee809e2bba73745d6150c
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2022-07-16 18:19:25 +0200

    Attempt to use codecov with clang

--- cmake/coverage.cmake
+++ cmake/coverage.cmake
@@ -1,33 +1,32 @@
 # ---- Variables ----
 
-# We use variables separate from what CTest uses, because those have
-# customization issues
 set(
-    COVERAGE_TRACE_COMMAND
-    lcov -c -q
-    -o "${PROJECT_BINARY_DIR}/coverage.info"
-    -d "${PROJECT_BINARY_DIR}"
-    --include "${PROJECT_SOURCE_DIR}/*"
-    CACHE STRING
-    "; separated command to generate a trace for the 'coverage' target"
+        COVERAGE_TRACE_COMMAND
+        LLVM_PROFILE_FILE="${PROJECT_BINARY_DIR}/test.profraw" "${PROJECT_BINARY_DIR}/RhythmGame_test"
+)
+
+
+set(
+        COVERAGE_MERGE_COMMAND
+        llvm-profdata merge -sparse "${PROJECT_BINARY_DIR}/test.profraw" -o "${PROJECT_BINARY_DIR}/test.profdata"
+        CACHE STRING
+        "; separated command to generate a trace for the 'coverage' target"
 )
 
 set(
-    COVERAGE_HTML_COMMAND
-    genhtml --legend -f -q
-    "${PROJECT_BINARY_DIR}/coverage.info"
-    -p "${PROJECT_SOURCE_DIR}"
-    -o "${PROJECT_BINARY_DIR}/coverage_html"
-    CACHE STRING
-    "; separated command to generate an HTML report for the 'coverage' target"
+        COVERAGE_SAVE_COMMAND
+        llvm-cov show "${PROJECT_BINARY_DIR}/RhythmGame_test" -instr-profile="${PROJECT_BINARY_DIR}/test.profdata" > coverage.info
+        CACHE STRING
+        "; separated command to generate an HTML report for the 'coverage' target"
 )
 
 # ---- Coverage target ----
 
 add_custom_target(
-    coverage
-    COMMAND ${COVERAGE_TRACE_COMMAND}
-    COMMAND ${COVERAGE_HTML_COMMAND}
-    COMMENT "Generating coverage report"
-    VERBATIM
+        coverage
+        COMMAND ${COVERAGE_TRACE_COMMAND}
+        COMMAND ${COVERAGE_MERGE_COMMAND}
+        COMMAND ${COVERAGE_SAVE_COMMAND}
+        COMMENT "Generating coverage report"
+        VERBATIM
 )
