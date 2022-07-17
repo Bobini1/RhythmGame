d7af3ec7866de5faa220b4ea7cd0e70ae5b886f7
Xyloo <s95408@pollub.edu.pl>
2022-07-17 20:41:40 +0200

    Match LLVM and Clang version to 12

--- cmake/coverage.cmake
+++ cmake/coverage.cmake
@@ -4,4 +4,5 @@ add_custom_target(
         coverage
         COMMAND llvm-profdata merge ${PROJECT_BINARY_DIR}/test/default.profraw -o ${PROJECT_BINARY_DIR}/test/test.profdata && llvm-cov show ${PROJECT_BINARY_DIR}/RhythmGame --instr-profile=${PROJECT_BINARY_DIR}/test/test.profdata > coverage.info
         COMMENT "Generating coverage report"
+        VERBATIM
 )
