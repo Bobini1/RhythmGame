8bd8af4974e15cf5a31b76c0f0ea618ba48f064c
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2022-07-17 21:57:37 +0200

    Add a new test for artist field

--- test/BmsChartReader.test.cpp
+++ test/BmsChartReader.test.cpp
@@ -13,5 +13,15 @@ TEST_CASE("Check if Title is parsed correctly", "[single-file]")
     auto testString = "#TITLE END TIME"s;
     auto res = reader.readBmsChart(testString);
     REQUIRE(static_cast<const std::string&>(res.getChartInfo().title) ==
-            ""s); // FIXME
+            "END TIME"s);
+}
+
+TEST_CASE("Check if Artist is parsed correctly", "[single-file]")
+{
+    using namespace std::literals::string_literals;
+    auto reader = charts::chart_readers::bms::BmsChartReader{};
+    auto testString = "#ARTIST cres"s;
+    auto res = reader.readBmsChart(testString);
+    REQUIRE(static_cast<const std::string&>(res.getChartInfo().artist) ==
+            "cres"s);
 }
