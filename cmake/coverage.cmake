c4d17374325df9908f1f179d6e694a2debe92cc2
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2022-07-17 16:28:32 +0200

    Fix coverage commands (remove verbatim)

--- cmake/coverage.cmake
+++ cmake/coverage.cmake
@@ -1,29 +1,24 @@
 # ---- Variables ----
 
-message(STATUS "raw coverage status location: ${PROJECT_BINARY_DIR}/default.profraw")
-
-set(
-        SHOW_DIRS
-        find ${PROJECT_BINARY_DIR} -name *.profraw
-)
-
 set(
         COVERAGE_TRACE_COMMAND
         "${PROJECT_BINARY_DIR}/test/RhythmGame_test"
+        CACHE STRING
+        "; tracing for the 'coverage' target"
 )
 
 set(
         COVERAGE_MERGE_COMMAND
-        llvm-profdata merge -sparse "${PROJECT_BINARY_DIR}/default.profraw" -o "${PROJECT_BINARY_DIR}/test/test.profdata"
+        llvm-profdata merge -sparse "${PROJECT_BINARY_DIR}/test/default.profraw" "${PROJECT_BINARY_DIR}/default.profraw" -o "${PROJECT_BINARY_DIR}/test/test.profdata"
         CACHE STRING
-        "; separated command to generate a trace for the 'coverage' target"
+        "; merging traces for the 'coverage' target"
 )
 
 set(
         COVERAGE_SAVE_COMMAND
-        llvm-cov show "${PROJECT_BINARY_DIR}/test/RhythmGame_test" -instr-profile="${PROJECT_BINARY_DIR}/test/test.profdata" > coverage.info
+        "llvm-cov show ${PROJECT_BINARY_DIR}/RhythmGame -instr-profile=${PROJECT_BINARY_DIR}/test/test.profdata"
         CACHE STRING
-        "; separated command to generate an HTML report for the 'coverage' target"
+        "; save report for the 'coverage' target"
 )
 
 # ---- Coverage target ----
@@ -31,9 +26,7 @@ set(
 add_custom_target(
         coverage
         COMMAND ${COVERAGE_TRACE_COMMAND}
-        COMMAND ${SHOW_DIRS}
         COMMAND ${COVERAGE_MERGE_COMMAND}
         COMMAND ${COVERAGE_SAVE_COMMAND}
         COMMENT "Generating coverage report"
-        VERBATIM
 )
