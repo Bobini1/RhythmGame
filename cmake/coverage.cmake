b614a9a7f9c10e55f87ae00082121f7d4c5359d2
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2022-07-17 16:42:10 +0200

    Remove non-existant profraw

--- cmake/coverage.cmake
+++ cmake/coverage.cmake
@@ -1,15 +1,8 @@
 # ---- Variables ----
 
 set(
-        COVERAGE_TRACE_COMMAND
-        "${PROJECT_BINARY_DIR}/test/RhythmGame_test"
-        CACHE STRING
-        "; tracing for the 'coverage' target"
-)
-
-set(
         COVERAGE_MERGE_COMMAND
-        llvm-profdata merge -sparse "${PROJECT_BINARY_DIR}/test/default.profraw" "${PROJECT_BINARY_DIR}/default.profraw" -o "${PROJECT_BINARY_DIR}/test/test.profdata"
+        llvm-profdata merge -sparse "${PROJECT_BINARY_DIR}/test/default.profraw" -o "${PROJECT_BINARY_DIR}/test/test.profdata"
         CACHE STRING
         "; merging traces for the 'coverage' target"
 )
@@ -25,7 +18,6 @@ set(
 
 add_custom_target(
         coverage
-        COMMAND ${COVERAGE_TRACE_COMMAND}
         COMMAND ${COVERAGE_MERGE_COMMAND}
         COMMAND ${COVERAGE_SAVE_COMMAND}
         COMMENT "Generating coverage report"
