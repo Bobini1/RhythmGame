38ef25a58025f68c6a060d19a5a9a92d0be0ec9c
Xyloo <s95408@pollub.edu.pl>
2022-07-17 21:02:38 +0200

    Actually run LLVM-12

--- cmake/coverage.cmake
+++ cmake/coverage.cmake
@@ -2,7 +2,7 @@
 
 add_custom_target(
         coverage
-        COMMAND llvm-profdata merge ${PROJECT_BINARY_DIR}/test/default.profraw -o ${PROJECT_BINARY_DIR}/test/test.profdata && llvm-cov show ${PROJECT_BINARY_DIR}/RhythmGame --instr-profile=${PROJECT_BINARY_DIR}/test/test.profdata > coverage.info
+        COMMAND llvm-profdata-12 merge ${PROJECT_BINARY_DIR}/test/default.profraw -o ${PROJECT_BINARY_DIR}/test/test.profdata && llvm-cov-12 show ${PROJECT_BINARY_DIR}/RhythmGame --instr-profile=${PROJECT_BINARY_DIR}/test/test.profdata > coverage.info
         COMMENT "Generating coverage report"
         VERBATIM
 )
