6accb59d57a05c6a2b1696dc54dd2fbf09359493
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2022-07-21 20:58:17 +0200

    Tested nested random.

--- test/BmsChartReader.test.cpp
+++ test/BmsChartReader.test.cpp
@@ -41,16 +41,7 @@ TEST_CASE("Extra whitespace is ignored", "[single-file]")
     using namespace std::literals::string_literals;
     auto reader = charts::chart_readers::BmsChartReader{};
     auto testString = " #ARTIST   cres   \n\n #TITLE     END TIME  \n"s;
-    // measure time
-    auto start = std::chrono::high_resolution_clock::now();
-
     auto res = reader.readBmsChart(testString);
-
-    auto end = std::chrono::high_resolution_clock::now();
-    auto duration =
-      std::chrono::duration_cast<std::chrono::microseconds>(end - start)
-        .count();
-    std::cout << "Duration: " << duration << " microseconds" << std::endl;
     REQUIRE(res.getArtist() == "cres"s);
     REQUIRE(res.getTitle() == "END TIME"s);
 }
@@ -131,4 +122,40 @@ TEST_CASE("Random blocks get parsed correctly", "[single-file]")
     REQUIRE(res.randomBlocks[0].second->size() == 1);
     REQUIRE(res.randomBlocks[0].second->contains(5));
     REQUIRE(res.randomBlocks[0].second->begin()->second->title == "44river"s);
+}
+
+TEST_CASE("Nested random blocks", "[single-file]")
+{
+
+    using namespace std::literals::string_literals;
+    auto reader = charts::chart_readers::BmsChartReader{};
+    auto testString =
+      "#RANDOM 5\n#IF 5\n#TITLE 44river\n#RANDOM 1\n#IF 1\n#ARTIST -45\n#ENDIF\n#ENDRANDOM\n#ENDIF"s;
+    auto res = reader.readBmsChartTags(testString);
+    REQUIRE(res.title == std::optional<std::string>{});
+    REQUIRE(res.artist == std::optional<std::string>{});
+    REQUIRE(res.bpm == std::optional<double>{});
+    REQUIRE(res.randomBlocks.size() == 1);
+    REQUIRE(res.randomBlocks[0].first == std::uniform_int_distribution(0L, 5L));
+    REQUIRE(res.randomBlocks[0].second->size() == 1);
+    REQUIRE(res.randomBlocks[0].second->contains(5));
+    REQUIRE(res.randomBlocks[0].second->begin()->second->title == "44river"s);
+    REQUIRE(res.randomBlocks[0].second->begin()->second->randomBlocks.size() ==
+            1);
+    REQUIRE(
+      res.randomBlocks[0].second->begin()->second->randomBlocks[0].first ==
+      std::uniform_int_distribution(0L, 1L));
+    REQUIRE(res.randomBlocks[0]
+              .second->begin()
+              ->second->randomBlocks[0]
+              .second->size() == 1);
+    REQUIRE(res.randomBlocks[0]
+              .second->begin()
+              ->second->randomBlocks[0]
+              .second->contains(1) == 1);
+    REQUIRE(res.randomBlocks[0]
+              .second->begin()
+              ->second->randomBlocks[0]
+              .second->begin()
+              ->second->artist == "-45"s);
 }
