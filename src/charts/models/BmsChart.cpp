69c693fd549577940bac8502fdd4070900b9544e
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2022-07-24 18:01:34 +0200

    Change nil to lua_nil to help macos...

--- src/charts/models/BmsChart.cpp
+++ src/charts/models/BmsChart.cpp
@@ -3,7 +3,6 @@
 //
 
 #include "BmsChart.h"
-#include <sol/types.hpp>
 charts::models::BmsChart::BmsChart(Tags tags)
   : tags(std::move(tags))
 {
@@ -15,31 +14,31 @@ charts::models::BmsChart::writeFullData(
     if (tags.title) {
         writer.writeVar("getTitle", [this] { return *tags.title; });
     } else {
-        writer.writeVar("getTitle", [] { return sol::nil; });
+        writer.writeVar("getTitle", [] { return sol::lua_nil; });
     }
     if (tags.artist) {
         writer.writeVar("getArtist", [this] { return *tags.artist; });
     } else {
-        writer.writeVar("getArtist", [] { return sol::nil; });
+        writer.writeVar("getArtist", [] { return sol::lua_nil; });
     }
     if (tags.bpm) {
         writer.writeVar("getBpm", [this] { return *tags.bpm; });
     } else {
-        writer.writeVar("getBpm", [] { return sol::nil; });
+        writer.writeVar("getBpm", [] { return sol::lua_nil; });
     }
     if (tags.subTitle) {
         writer.writeVar("getSubTitle", [this] { return *tags.subTitle; });
     } else {
-        writer.writeVar("getSubTitle", [] { return sol::nil; });
+        writer.writeVar("getSubTitle", [] { return sol::lua_nil; });
     }
     if (tags.subArtist) {
         writer.writeVar("getSubArtist", [this] { return *tags.subArtist; });
     } else {
-        writer.writeVar("getSubArtist", [] { return sol::nil; });
+        writer.writeVar("getSubArtist", [] { return sol::lua_nil; });
     }
     if (tags.genre) {
         writer.writeVar("getGenre", [this] { return *tags.genre; });
     } else {
-        writer.writeVar("getGenre", [] { return sol::nil; });
+        writer.writeVar("getGenre", [] { return sol::lua_nil; });
     }
 }
