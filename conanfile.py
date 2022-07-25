73ca4a9faf0d1e6295f143f826f83918f815888c
Xyloo <s95408@pollub.edu.pl>
2022-07-25 19:32:20 +0200

    Add folly to dependencies

--- conanfile.py
+++ conanfile.py
@@ -4,13 +4,14 @@ from conan import ConanFile
 class Recipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeToolchain", "CMakeDeps", "VirtualRunEnv"
-    build_requires = "cmake/3.23.0"
+    build_requires = "cmake/3.23.2"
 
     def layout(self):
         self.folders.generators = "conan"
 
     def requirements(self):
-        self.requires("openssl/1.1.1o")
+        self.requires("cmake/3.23.2")
+        self.requires("openssl/1.1.1q")
         self.requires("sol2/3.3.0")
         self.requires("sqlitecpp/3.1.1")
         self.requires("boost/1.79.0")
@@ -20,7 +21,7 @@ class Recipe(ConanFile):
         self.requires("ms-gsl/4.0.0")
         self.requires("type_safe/0.2.2")
         self.requires("luajit/2.0.5")
-        # self.requires("folly/2022.01.31.00")
+        self.requires("folly/2022.01.31.00")
 
         # Testing only dependencies below
         self.requires("catch2/3.0.1")
