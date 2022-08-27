d36fc0c2a94764790f51625e48faf2cb78470d12
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2022-08-27 19:43:15 +0200

    Use revisions for old libpng

--- conanfile.py
+++ conanfile.py
@@ -11,6 +11,7 @@ class Recipe(ConanFile):
 
     def requirements(self):
         self.requires("freetype/2.12.1@#0bbb81805455cee1339cfd08a4682f1b")
+        self.requires("libpng/1.6.37@#abacd98afb859828aa70354fa7d55285")
         self.requires("sol2/3.3.0")
         self.requires("sqlitecpp/3.1.1")
         self.requires("boost/1.79.0")
