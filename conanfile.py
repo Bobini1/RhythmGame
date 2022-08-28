02d523a7ad3569da2cdcd22c928a7a42a7944233
Xyloo <s95408@pollub.edu.pl>
2022-08-28 21:04:54 +0200

    Add libbacktrace as a dependency

--- conanfile.py
+++ conanfile.py
@@ -24,6 +24,7 @@ class Recipe(ConanFile):
         self.requires("cocoyaxi/2.0.3")
         self.requires("sfml/2.5.1")
         self.requires("di/1.2.0")
+        self.requires("libbacktrace/cci.20210118")
 
         # Testing only dependencies below
         self.requires("catch2/3.0.1")
