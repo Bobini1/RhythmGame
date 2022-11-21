6d8b2dd50437506ba8ac2d21d22777d0782ebca1
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2022-11-21 02:58:33 +0100

    Fix includes

--- test/setupState.h
+++ test/setupState.h
@@ -1,11 +1,11 @@
 #ifndef RHYTHMGAME_SETUPSTATE_H
 #define RHYTHMGAME_SETUPSTATE_H
 
-#include "../../.conan/data/sfml/2.5.1/_/_/package/8c725b9a48d15986d670001801d4f5f3c09d097d/include/SFML/Graphics/Texture.hpp"
-#include "../../.conan/data/sol2/3.3.0/_/_/package/5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9/include/sol/state.hpp"
-#include "../src/lua/Bootstrapper.h"
-#include "../src/resource_managers/TextureLoaderImpl.h"
-#include "../src/resource_managers/FontLoaderImpl.h"
+#include <SFML/Graphics/Texture.hpp>
+#include <sol/state.hpp>
+#include "lua/Bootstrapper.h"
+#include "resource_managers/TextureLoaderImpl.h"
+#include "resource_managers/FontLoaderImpl.h"
 
 inline auto
 getStateWithAllDefinitions() -> sol::state
