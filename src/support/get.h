9ab24b30a5240b6ac091ea4a22d89640a0e9a2df
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2023-06-15 09:34:42 +0200

    Don't use ADL for get
    I couldn't get it right

--- src/support/get.h
+++ src/support/get.h
@@ -7,27 +7,20 @@
 
 #include <boost/pfr/tuple_size.hpp>
 #include <boost/pfr/core.hpp>
+
 namespace support {
 template<std::size_t N, typename T>
-constexpr auto stdGettable() -> bool
+constexpr auto
+get(T&& t) -> auto&
+    requires requires { std::get<N>(std::declval<T>()); }
 {
-    using std::get;
-    return requires {
-        get<N>(std::declval<T>());
-    };
+    return std::get<N>(t);
 }
 
 template<std::size_t N, typename T>
 constexpr auto
-get(T&& t) -> auto& requires (stdGettable<N, T>())
-{
-    using std::get;
-    return get<N>(t);
-}
-
-template<std::size_t N, typename T>
-constexpr auto
-get(T&& aggregate) -> auto& requires std::is_aggregate_v<std::remove_cvref_t<T>>
+get(T&& aggregate) -> auto&
+    requires std::is_aggregate_v<std::remove_cvref_t<T>>
 {
     return boost::pfr::get<N>(aggregate);
 }
