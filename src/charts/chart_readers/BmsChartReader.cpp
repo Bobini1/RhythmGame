388a493597a957781abdf32e8d94c64a88695c18
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2023-09-18 13:21:36 +0200

    Convert identifiers tolower

--- src/charts/chart_readers/BmsChartReader.cpp
+++ src/charts/chart_readers/BmsChartReader.cpp
@@ -40,9 +40,75 @@ struct TextTag
       dsl::capture(dsl::until(dsl::unicode::newline).or_eof());
 };
 
+auto
+tolower(char p) -> char
+{
+    switch (p) {
+        case 'A':
+            return 'a';
+        case 'B':
+            return 'b';
+        case 'C':
+            return 'c';
+        case 'D':
+            return 'd';
+        case 'E':
+            return 'e';
+        case 'F':
+            return 'f';
+        case 'G':
+            return 'g';
+        case 'H':
+            return 'h';
+        case 'I':
+            return 'i';
+        case 'J':
+            return 'j';
+        case 'K':
+            return 'k';
+        case 'L':
+            return 'l';
+        case 'M':
+            return 'm';
+        case 'N':
+            return 'n';
+        case 'O':
+            return 'o';
+        case 'P':
+            return 'p';
+        case 'Q':
+            return 'q';
+        case 'R':
+            return 'r';
+        case 'S':
+            return 's';
+        case 'T':
+            return 't';
+        case 'U':
+            return 'u';
+        case 'V':
+            return 'v';
+        case 'W':
+            return 'w';
+        case 'X':
+            return 'x';
+        case 'Y':
+            return 'y';
+        case 'Z':
+            return 'z';
+        default:
+            return p;
+    };
+}
+
 struct Identifier
 {
-    static constexpr auto value = lexy::as_string<std::string>;
+    static constexpr auto value = lexy::callback<std::string>([](auto&& str) {
+        auto strLower = std::string{};
+        strLower.resize(str.size());
+        std::transform(str.begin(), str.end(), strLower.begin(), tolower);
+        return strLower;
+    });
     static constexpr auto rule =
       dsl::capture(dsl::token(dsl::twice(dsl::ascii::alnum)));
 };
