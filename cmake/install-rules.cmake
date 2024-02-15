7c6a5898823bf68f152b51cba7a782312084a8ba
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2024-02-15 07:11:58 +0100

    Only install some assets

--- cmake/install-rules.cmake
+++ cmake/install-rules.cmake
@@ -3,8 +3,21 @@ install(
         RUNTIME COMPONENT RhythmGame_Runtime
 )
 
-install(DIRECTORY assets/ DESTINATION assets/
+install(DIRECTORY assets/avatars/ DESTINATION assets/avatars/
         COMPONENT RhythmGame_Runtime)
+
+
+install(DIRECTORY assets/themes/ DESTINATION assets/themes/
+        COMPONENT RhythmGame_Runtime)
+
+if (WIN32)
+
+    install(FILES alsoft.ini DESTINATION "${CMAKE_INSTALL_BINDIR}"
+            COMPONENT RhythmGame_Runtime)
+else ()
+    install(FILES alsoft.ini DESTINATION "${CMAKE_INSTALL_BINDIR}" RENAME alsoft.conf
+            COMPONENT RhythmGame_Runtime)
+endif ()
 
 if (PROJECT_IS_TOP_LEVEL)
     include(CPack)
