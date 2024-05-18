16566ca4deb7e7989d9a1553c9ce5c2579e8e957
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2024-05-18 21:20:26 +0200

    Add TaoQuick

--- cmake/install-rules.cmake
+++ cmake/install-rules.cmake
@@ -8,10 +8,19 @@ install(DIRECTORY assets/avatars/ DESTINATION assets/avatars/
 
 
 install(DIRECTORY assets/themes/ DESTINATION assets/themes/
-        COMPONENT RhythmGame_Runtime)
+        COMPONENT RhythmGame_Runtime
+        PATTERN "assets/themes/Default/scripts/third_party/*" EXCLUDE)
+
+file(GLOB_RECURSE third_party_dirs FOLLOW_SYMLINKS RELATIVE ${CMAKE_SOURCE_DIR}/assets/themes/Default/scripts/third_party
+        ${CMAKE_SOURCE_DIR}/assets/themes/Default/scripts/third_party/*)
+message(STATUS "third_party_dirs: ${third_party_dirs}")
+foreach (file IN LISTS third_party_dirs)
+    get_filename_component(dir ${file} DIRECTORY)
+    install(FILES assets/themes/Default/scripts/third_party/${file} DESTINATION assets/themes/Default/scripts/third_party/${dir}
+            COMPONENT RhythmGame_Runtime)
+endforeach ()
 
 if (WIN32)
-
     install(FILES alsoft.ini DESTINATION "${CMAKE_INSTALL_BINDIR}"
             COMPONENT RhythmGame_Runtime)
 else ()
