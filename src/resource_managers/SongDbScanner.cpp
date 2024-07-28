e9ef493834e17866e4c7f3e791a8eab9d1cdd261
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2024-07-28 12:59:04 +0200

    Use direct ntdll calls on Windows

--- src/resource_managers/SongDbScanner.cpp
+++ src/resource_managers/SongDbScanner.cpp
@@ -11,6 +11,11 @@
 
 #include <qthreadpool.h>
 #include <spdlog/stopwatch.h>
+#ifdef _WIN32
+#include <windows.h>
+#include <winternl.h>
+#include <ntstatus.h>
+#endif
 #include <llfio.hpp>
 
 namespace llfio = LLFIO_V2_NAMESPACE;
@@ -104,84 +109,125 @@ addPreviewFileToDb(db::SqliteCppDb& db,
     statement.bind(2, support::pathToUtfString(directory / ""));
     statement.execute();
 }
+#ifdef _WIN32
+using NtQueryDirectoryFile_t = NTSTATUS(NTAPI *)(_In_ HANDLE FileHandle, _In_opt_ HANDLE Event, _In_opt_ PIO_APC_ROUTINE ApcRoutine,
+                                                _In_opt_ PVOID ApcContext, _Out_ PIO_STATUS_BLOCK IoStatusBlock, _Out_ PVOID FileInformation,
+                                                _In_ ULONG Length, _In_ FILE_INFORMATION_CLASS FileInformationClass, _In_ BOOLEAN ReturnSingleEntry,
+                                                _In_opt_ PUNICODE_STRING FileName, _In_ BOOLEAN RestartScan);
+
+typedef struct _FILE_DIRECTORY_INFORMATION {
+    ULONG NextEntryOffset;
+    ULONG FileIndex;
+    LARGE_INTEGER CreationTime;
+    LARGE_INTEGER LastAccessTime;
+    LARGE_INTEGER LastWriteTime;
+    LARGE_INTEGER ChangeTime;
+    LARGE_INTEGER EndOfFile;
+    LARGE_INTEGER AllocationSize;
+    ULONG FileAttributes;
+    ULONG FileNameLength;
+    WCHAR FileName[1];
+} FILE_DIRECTORY_INFORMATION, *PFILE_DIRECTORY_INFORMATION;
 
 void
 scanFolder(std::filesystem::path directory,
-           llfio::directory_handle dirHandle,
            std::filesystem::path parentDirectory,
-           std::vector<llfio::directory_handle::buffer_type>& buffer,
            QThreadPool& threadPool,
            db::SqliteCppDb& db,
            const QString& root,
            std::function<void(QString)> updateCurrentScannedFolder,
            std::atomic_bool* stop)
 {
+    static HMODULE ntdllh = GetModuleHandleA("NTDLL.DLL");
+    static auto NtQueryDirectoryFile = reinterpret_cast<NtQueryDirectoryFile_t>(GetProcAddress(ntdllh, "NtQueryDirectoryFile"));
+    auto isb = IO_STATUS_BLOCK{};
+    memset(&isb, 0, sizeof(isb));
+    isb.Status = -1;
+    static constexpr auto max_bytes = 65536;
+    char buffer[65536];
+    HANDLE hDirectory = CreateFileW(
+      directory.c_str(),
+      FILE_LIST_DIRECTORY,
+      FILE_SHARE_READ,
+      NULL,
+      OPEN_EXISTING,
+      FILE_FLAG_BACKUP_SEMANTICS,
+      NULL
+    );
+
+
     updateCurrentScannedFolder(support::pathToQString(directory));
     auto directoriesToScan = std::vector<std::filesystem::path>{};
     auto isSongDirectory = false;
 
-    // Very similar to reading from a file handle, we need
-    // to achieve a single snapshot read to be race free.
-    llfio::directory_handle::buffers_type entries(buffer);
-    //auto NtQueryDirectoryFile = reinterpret_cast<NtQueryDirectoryFile_t>(GetProcAddress(ntdllh, "NtQueryDirectoryFile"))
-    for(;;)
-    {
-        //NtQueryDirectoryFile()
-        entries = dirHandle.read(
-                      {std::move(entries)}   // buffers to fill
-                      ).value();               // If failed, throw a filesystem_error exception
+    while (true) {
+        auto status = NtQueryDirectoryFile(
+          hDirectory,
+          NULL,
+          NULL,
+          NULL,
+          &isb,
+          buffer,
+          sizeof(buffer),
+          FileDirectoryInformation,
+          FALSE,
+          NULL,
+          false
+        );
 
-        // If there were fewer entries in the directory than buffers
-        // passed in, we are done.
-        if(entries.done())
-        {
+        if (status == STATUS_NO_MORE_FILES) {
+            break; // No more files to process
+        }
+
+        if (status != STATUS_SUCCESS && status != STATUS_BUFFER_OVERFLOW) {
+            spdlog::error("NtQueryDirectoryFile failed. NTSTATUS: {:#x}\n", status);
             break;
         }
-        // Otherwise double the size of the buffer
-        buffer.resize(buffer.size() << 1);
-        // Set the next read attempt to use the newly enlarged buffer.
-        // buffers_type may cache internally reusable state depending
-        // on platform, to efficiently reuse that state pass in the
-        // old entries by rvalue ref.
-        entries = { buffer, std::move(entries) };
-    }
 
-    for (const auto& entry : entries) {
         if (*stop) {
             break;
         }
-        const auto& path = entry.leafname;
-        if (entry.stat.st_type == llfio::filesystem::file_type::directory && !isSongDirectory) {
-            directoriesToScan.push_back(path.path());
-        } else if (auto extension = path.extension();
-                   extension.compare(".bms") == 0 || extension.compare(".bme") == 0  ||
-                   extension.compare(".bml") == 0  || extension.compare(".pms") == 0 ) {
-            isSongDirectory = true;
-            directoriesToScan.clear();
-            if (extension.compare(".pms") == 0) {
-                continue;
+
+        auto* fileInfo = reinterpret_cast<PFILE_DIRECTORY_INFORMATION>(buffer);
+
+        do {
+            auto path = std::wstring_view{fileInfo->FileName, fileInfo->FileNameLength / sizeof(WCHAR)};
+            if (fileInfo->FileAttributes & FILE_ATTRIBUTE_DIRECTORY && path != L"." && path != L"..") {
+                if (!isSongDirectory) {
+                    directoriesToScan.emplace_back(path);
+                }
+            } else if (auto extension = llfio::path_view(path).extension();
+                       extension.compare(".bms") == 0 || extension.compare(".bme") == 0  ||
+                       extension.compare(".bml") == 0  || extension.compare(".pms") == 0 ) {
+                isSongDirectory = true;
+                directoriesToScan.clear();
+                if (extension.compare(".pms") == 0) {
+                    continue;
+                }
+                loadChart(
+                  threadPool, db, support::pathToQString(parentDirectory), directory / path);
+                // converting to string should not break stuff even on windows
+                // in this case
+            } else if (path.starts_with(L"preview") &&
+                                                    (extension.compare(".mp3") == 0 || extension.compare(".ogg") == 0  ||
+                                                     extension.compare(".wav") == 0  || extension.compare(".flac") == 0 )) {
+                threadPool.start([&db, directory, path = std::filesystem::path(path)] {
+                    addPreviewFileToDb(db, directory, path);
+                });
             }
-            loadChart(
-              threadPool, db, support::pathToQString(parentDirectory), directory / path);
-            // converting to string should not break stuff even on windows
-            // in this case
-        } else if (auto pathCopy = path.path(); pathCopy.filename().string().starts_with("preview") &&
-                   (extension.compare(".mp3") == 0 || extension.compare(".ogg") == 0  ||
-                    extension.compare(".wav") == 0  || extension.compare(".flac") == 0 )) {
-            threadPool.start([&db, directory, pathCopy = std::move(pathCopy)] {
-                addPreviewFileToDb(db, directory, pathCopy);
-            });
-        }
+
+            if (fileInfo->NextEntryOffset == 0) {
+                break;
+            }
+            fileInfo = (PFILE_DIRECTORY_INFORMATION)((BYTE*)fileInfo + fileInfo->NextEntryOffset);
+        } while (true);
     }
     for (const auto& entry : directoriesToScan) {
         if (*stop) {
             break;
         }
-        auto entryHandle = llfio::directory(dirHandle, entry).value();
         scanFolder(directory / entry,
-                   std::move(entryHandle),
                    directory,
-                   buffer,
                    threadPool,
                    db,
                    root,
@@ -193,6 +239,64 @@ scanFolder(std::filesystem::path directory,
           threadPool, db, root, support::pathToQString(parentDirectory));
     }
 }
+#else
+void
+scanFolder(std::filesystem::path directory,
+           std::filesystem::path parentDirectory,
+           QThreadPool& threadPool,
+           db::SqliteCppDb& db,
+           const QString& root,
+           std::function<void(QString)> updateCurrentScannedFolder,
+           std::atomic_bool* stop)
+{
+    updateCurrentScannedFolder(support::pathToQString(directory));
+    auto directoriesToScan = std::vector<std::filesystem::path>{};
+    auto isSongDirectory = false;
+    for (const auto& entry : std::filesystem::directory_iterator(directory)) {
+        if (*stop) {
+            break;
+        }
+        const auto& path = entry.path();
+        if (is_directory(entry) && !isSongDirectory) {
+            directoriesToScan.push_back(path);
+        } else if (auto extension = path.extension();
+                   extension == ".bms" || extension == ".bme" ||
+                   extension == ".bml" || extension == ".pms") {
+            isSongDirectory = true;
+            directoriesToScan.clear();
+            if (extension == ".pms") {
+                continue;
+            }
+            loadChart(
+              threadPool, db, support::pathToQString(parentDirectory), path);
+            // converting to string should not break stuff even on windows
+            // in this case
+        } else if (path.filename().string().starts_with("preview") &&
+                   (extension == ".mp3" || extension == ".ogg" ||
+                    extension == ".wav" || extension == ".flac")) {
+            threadPool.start([&db, directory, path] {
+                addPreviewFileToDb(db, directory, path);
+            });
+        }
+    }
+    for (const auto& entry : directoriesToScan) {
+        if (*stop) {
+            break;
+        }
+        scanFolder(entry,
+                   directory,
+                   threadPool,
+                   db,
+                   root,
+                   updateCurrentScannedFolder,
+                   stop);
+    }
+    if (isSongDirectory) {
+        addDirToParentDirs(
+          threadPool, db, root, support::pathToQString(parentDirectory));
+    }
+}
+#endif
 
 void
 SongDbScanner::scanDirectory(
@@ -205,15 +309,11 @@ SongDbScanner::scanDirectory(
     try {
         if (is_directory(directory)) {
             const auto root = support::pathToQString(directory);
-            auto p = llfio::path(directory).value();
-            auto entryHandle = llfio::directory(p, "").value();
 
             // Read up to ten directory_entry
             std::vector<llfio::directory_handle::buffer_type> buffer(20000);
             scanFolder(directory,
-                       std::move(entryHandle),
                        {},
-                       buffer,
                        threadPool,
                        *db,
                        root,
