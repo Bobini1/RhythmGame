10c24b3990a531f63d9cc7a760a705f336cc72c7
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2024-08-11 19:30:20 +0200

    Lehmer code

--- src/support/LehmerCode.cpp
+++ src/support/LehmerCode.cpp
@@ -5,4 +5,56 @@
 #include "LehmerCode.h"
 
 namespace support {
-} // support
+
+uint64_t factorial(int n) {
+    auto fact = uint64_t{ 1 };
+    for (int i = 2; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
+uint64_t encodePermutation(std::span<const int> permutation) {
+    int n = permutation.size();
+    std::vector<bool> used(n, false);
+    uint64_t lehmerCode = 0;
+
+    for (int i = 0; i < n; i++) {
+        int count = 0;
+        for (int j = 0; j < permutation[i]; j++) {
+            if (!used[j]) {
+                count++;
+            }
+        }
+        lehmerCode += count * factorial(n - i - 1);
+        used[permutation[i]] = true;
+    }
+
+    return lehmerCode;
+}
+
+QList<int> decodePermutation(uint64_t lehmerCode, int n) {
+    QList<int> permutation(n);
+    std::vector<int> availableElements(n);
+    
+    // Initialize the available elements
+    for (int i = 0; i < n; i++) {
+        availableElements[i] = i;
+    }
+    
+    for (int i = 0; i < n; i++) {
+        uint64_t factorialValue = factorial(n - i - 1);
+        int index = lehmerCode / factorialValue;
+        permutation[i] = availableElements[index];
+        
+        // Remove the used element
+        availableElements.erase(availableElements.begin() + index);
+        
+        // Update the Lehmer code
+        lehmerCode %= factorialValue;
+    }
+    
+    return permutation;
+}
+
+} // namespace support
--- src/support/LehmerCode.h
+++ src/support/LehmerCode.h
@@ -4,13 +4,13 @@
 
 #ifndef RHYTHMGAME_LEHMERCODE_H
 #define RHYTHMGAME_LEHMERCODE_H
+#include <cstdint>
+#include <QList>
+#include <span>
 
 namespace support {
-
-class LehmerCode
-{
-};
-
-} // support
+uint64_t encodePermutation(std::span<const int> permutation);
+QList<int> decodePermutation(uint64_t lehmerCode, int n);
+} // namespace support
 
 #endif // RHYTHMGAME_LEHMERCODE_H
