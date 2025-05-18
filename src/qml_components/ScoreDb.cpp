6c955af01a360cdb753fbaf856ce4b9d0deaca4d
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2025-05-18 02:06:55 +0200

    Order returned scores by timestamp

--- src/qml_components/ScoreDb.cpp
+++ src/qml_components/ScoreDb.cpp
@@ -24,29 +24,32 @@ ScoreDb::getScoresForMd5(const QList<QString>& md5s) const
         return reply;
     }
     QThreadPool::globalInstance()->start([this, md5s, reply]() mutable {
-        auto statement = scoreDb->createStatement("SELECT * "
-                                                  "FROM score "
-                                                  "JOIN replay_data ON score.guid = replay_data.score_guid "
-                                                  "JOIN gauge_history ON score.guid = gauge_history.score_guid "
-                                                  "WHERE score.md5 IN (" +
-                                                  QString("?, ").repeated(md5s.size()).chopped(2).toStdString() + ")");
+        auto statement = scoreDb->createStatement(
+          "SELECT * "
+          "FROM score "
+          "JOIN replay_data ON score.guid = replay_data.score_guid "
+          "JOIN gauge_history ON score.guid = gauge_history.score_guid "
+          "WHERE score.md5 IN (" +
+          QString("?, ").repeated(md5s.size()).chopped(2).toStdString() +
+          ") ORDER BY score.unix_timestamp DESC");
 
         for (int i = 0; i < md5s.size(); ++i) {
             statement.bind(i + 1, md5s[i].toStdString());
         }
 
-        const auto result =
-          statement.executeAndGetAll<std::tuple<gameplay_logic::BmsResult::BmsResultDto,
-        gameplay_logic::BmsReplayData::DTO, gameplay_logic::BmsGaugeHistory::DTO>>();
+        const auto result = statement.executeAndGetAll<
+          std::tuple<gameplay_logic::BmsResult::BmsResultDto,
+                     gameplay_logic::BmsReplayData::DTO,
+                     gameplay_logic::BmsGaugeHistory::DTO>>();
 
         QHash<QString, QList<gameplay_logic::BmsScore*>> groupedScores;
         auto mainThread = QCoreApplication::instance()->thread();
         for (const auto& row : result) {
             auto md5 = QString::fromStdString(std::get<0>(row).md5);
             groupedScores[md5].append(new gameplay_logic::BmsScore{
-                gameplay_logic::BmsResult::load(std::get<0>(row)),
-                gameplay_logic::BmsReplayData::load(std::get<1>(row)),
-                gameplay_logic::BmsGaugeHistory::load(std::get<2>(row))});
+              gameplay_logic::BmsResult::load(std::get<0>(row)),
+              gameplay_logic::BmsReplayData::load(std::get<1>(row)),
+              gameplay_logic::BmsGaugeHistory::load(std::get<2>(row)) });
             groupedScores[md5].back()->moveToThread(mainThread);
         }
 
@@ -54,8 +57,10 @@ ScoreDb::getScoresForMd5(const QList<QString>& md5s) const
         for (const auto& md5 : md5s) {
             scores.append(groupedScores.value(md5, {}));
         }
-        QMetaObject::invokeMethod(QCoreApplication::instance(), [reply, scores]() mutable {reply.setSuccess(scores);},
-                                  Qt::QueuedConnection);
+        QMetaObject::invokeMethod(
+          QCoreApplication::instance(),
+          [reply, scores]() mutable { reply.setSuccess(scores); },
+          Qt::QueuedConnection);
     });
     return reply;
 }
