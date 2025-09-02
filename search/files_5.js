130fbeb3d194177cfa34d7080dff26114dc59400
Bobini1 <Bobini1@users.noreply.github.com>
2025-09-02 12:44:57 +0000

    deploy: 5a17c68682b18858640d4e7a7636795faeb95a17

--- search/files_5.js
+++ search/files_5.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['filequery_2ecpp_0',['FileQuery.cpp',['../FileQuery_8cpp.html',1,'']]],
   ['filequery_2eh_1',['FileQuery.h',['../FileQuery_8h.html',1,'']]],
-  ['findassetsfolder_2ecpp_2',['FindAssetsFolder.cpp',['../FindAssetsFolder_8cpp.html',1,'']]],
-  ['findassetsfolder_2eh_3',['FindAssetsFolder.h',['../FindAssetsFolder_8h.html',1,'']]]
+  ['finddatafolder_2ecpp_2',['FindDataFolder.cpp',['../FindDataFolder_8cpp.html',1,'']]],
+  ['finddatafolder_2eh_3',['FindDataFolder.h',['../FindDataFolder_8h.html',1,'']]]
 ];
--- search/functions_5.js
+++ search/functions_5.js
@@ -4,7 +4,7 @@ var searchData=
   ['fillwithdefaults_1',['fillWithDefaults',['../namespaceresource__managers.html#afd50ac873ffb6f3786d8bc1853f5d53c',1,'resource_managers']]],
   ['filter_2',['Filter',['../classgameplay__logic_1_1Filter.html#a0893e2cda001462f86d4c1aab652e418',1,'gameplay_logic::Filter']]],
   ['filteracceptsrow_3',['filterAcceptsRow',['../classgameplay__logic_1_1BarlineFilter.html#aeec3fffaece0a4a793df222e58a6cb7a',1,'gameplay_logic::BarlineFilter']]],
-  ['findassetsfolder_4',['findAssetsFolder',['../namespaceresource__managers.html#ae7f2e06bccd108de7061c63358c87b19',1,'resource_managers']]],
+  ['finddatafolder_4',['findDataFolder',['../namespaceresource__managers.html#a328ed900df4f43724208e89d8e968f2d',1,'resource_managers']]],
   ['finish_5',['finish',['../structcharts_1_1TagsSink_1_1SinkCallback.html#ae104c5e6e5e9798acd87033143ae78f5',1,'charts::TagsSink::SinkCallback::finish()'],['../structcharts_1_1RandomSink_1_1RandomSinkCallback.html#af7afb49ff42921ad02ba55be33011217',1,'charts::RandomSink::RandomSinkCallback::finish()'],['../classgameplay__logic_1_1ChartRunner.html#a42b6cf59462b38b21aaca4160dd1f084',1,'gameplay_logic::ChartRunner::finish()'],['../classgameplay__logic_1_1Player.html#a70b452ca8edd0316e14d6afafd8bb2d0',1,'gameplay_logic::Player::finish()'],['../classgameplay__logic_1_1CourseRunner.html#a20c65437eab26ef8f14775e820f081da',1,'gameplay_logic::CourseRunner::finish()']]],
   ['fisheryatesshuffle_6',['fisherYatesShuffle',['../namespacesupport.html#a6b79ba387d451e0f812dfc86ccdc6b50',1,'support']]],
   ['flush_5f_7',['flush_',['../classsupport_1_1QtSink.html#aa6e54486c44af5c28ffa822343b3d1a5',1,'support::QtSink']]],
