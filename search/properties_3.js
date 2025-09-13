a8f2cae973cbc83b118926ca8193bdc72d9fc0cf
Bobini1 <Bobini1@users.noreply.github.com>
2025-09-13 15:59:19 +0000

    deploy: 44da6eac2106617318fe06aa338f5d1eda4d1e40

--- search/properties_3.js
+++ search/properties_3.js
@@ -1,11 +1,12 @@
 var searchData=
 [
-  ['denominator_0',['denominator',['../classgameplay__logic_1_1Snap.html#a72d8310b14bb16731f03c1da00388850',1,'gameplay_logic::Snap']]],
-  ['deviation_1',['deviation',['../classgameplay__logic_1_1BmsPoints.html#af065d142171b6f10f27afc50b33177a8',1,'gameplay_logic::BmsPoints']]],
-  ['device_2',['device',['../classinput_1_1Key.html#a1ddf7a5c8ea1860aa44588c9adc7c1d5',1,'input::Key::device'],['../classsounds_1_1AudioEngine.html#a013e4c84be49003ce1aaf77a055316bc',1,'sounds::AudioEngine::device']]],
-  ['devicenames_3',['deviceNames',['../classsounds_1_1AudioEngine.html#a0cfb686165007295c58eeef242dc0f63',1,'sounds::AudioEngine']]],
-  ['difficulty_4',['difficulty',['../classgameplay__logic_1_1ChartData.html#a5ee33547dc94fe2a08c949d25eab7202',1,'gameplay_logic::ChartData']]],
-  ['direction_5',['direction',['../classinput_1_1Key.html#a02f02ab94397c31f19395ba6e095268c',1,'input::Key']]],
-  ['directory_6',['directory',['../classgameplay__logic_1_1ChartData.html#a1ebfc239537260343195c0be0f4ce307',1,'gameplay_logic::ChartData']]],
-  ['dpoptions_7',['dpoptions',['../classgameplay__logic_1_1BmsResult.html#ae113d129f204b73a16e8a9b803f3fd6e',1,'gameplay_logic::BmsResult::dpOptions'],['../classgameplay__logic_1_1BmsResultCourse.html#ae4625fa885ef60cd9c0090b829a6dfa8',1,'gameplay_logic::BmsResultCourse::dpOptions'],['../classresource__managers_1_1GeneralVars.html#aa6d31437196872838978f6ab19918b1e',1,'resource_managers::GeneralVars::dpOptions']]]
+  ['debouncems_0',['debounceMs',['../classinput_1_1InputTranslator.html#ab7c53aa4db8117ac80d69fab3ce3e972',1,'input::InputTranslator']]],
+  ['denominator_1',['denominator',['../classgameplay__logic_1_1Snap.html#a72d8310b14bb16731f03c1da00388850',1,'gameplay_logic::Snap']]],
+  ['deviation_2',['deviation',['../classgameplay__logic_1_1BmsPoints.html#af065d142171b6f10f27afc50b33177a8',1,'gameplay_logic::BmsPoints']]],
+  ['device_3',['device',['../classinput_1_1Key.html#a1ddf7a5c8ea1860aa44588c9adc7c1d5',1,'input::Key::device'],['../classsounds_1_1AudioEngine.html#a013e4c84be49003ce1aaf77a055316bc',1,'sounds::AudioEngine::device']]],
+  ['devicenames_4',['deviceNames',['../classsounds_1_1AudioEngine.html#a0cfb686165007295c58eeef242dc0f63',1,'sounds::AudioEngine']]],
+  ['difficulty_5',['difficulty',['../classgameplay__logic_1_1ChartData.html#a5ee33547dc94fe2a08c949d25eab7202',1,'gameplay_logic::ChartData']]],
+  ['direction_6',['direction',['../classinput_1_1Key.html#a02f02ab94397c31f19395ba6e095268c',1,'input::Key']]],
+  ['directory_7',['directory',['../classgameplay__logic_1_1ChartData.html#a1ebfc239537260343195c0be0f4ce307',1,'gameplay_logic::ChartData']]],
+  ['dpoptions_8',['dpoptions',['../classgameplay__logic_1_1BmsResult.html#ae113d129f204b73a16e8a9b803f3fd6e',1,'gameplay_logic::BmsResult::dpOptions'],['../classgameplay__logic_1_1BmsResultCourse.html#ae4625fa885ef60cd9c0090b829a6dfa8',1,'gameplay_logic::BmsResultCourse::dpOptions'],['../classresource__managers_1_1GeneralVars.html#aa6d31437196872838978f6ab19918b1e',1,'resource_managers::GeneralVars::dpOptions']]]
 ];
