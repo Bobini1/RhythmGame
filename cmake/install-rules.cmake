af22ccbfa9515ba159a9614f8cc89ef390e20d68
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2025-09-15 19:31:57 +0200

    Allow more flexibility in install rules

--- cmake/install-rules.cmake
+++ cmake/install-rules.cmake
@@ -1,34 +1,39 @@
-install(
-        TARGETS RhythmGame_exe
-        RUNTIME_DEPENDENCY_SET RuntimeDeps
-        RUNTIME COMPONENT RhythmGame_Runtime
-)
+option(PORTABLE_BUILD "Deploy libraries and provide RhythmGame.sh" ON)
+if (INSTALL_RUNTIME_DEPS)
+    install(
+            TARGETS RhythmGame_exe
+            RUNTIME_DEPENDENCY_SET RuntimeDeps
+            RUNTIME COMPONENT RhythmGame_Runtime
+    )
 
-install(DIRECTORY data/avatars/ DESTINATION data/avatars/
-        COMPONENT RhythmGame_Runtime)
+    qt_generate_deploy_qml_app_script(
+            TARGET RhythmGame_exe
+            OUTPUT_SCRIPT deploy_script
+            NO_UNSUPPORTED_PLATFORM_ERROR
+            DEPLOY_TOOL_OPTIONS "--qmldir \"${CMAKE_SOURCE_DIR}/data/themes/Default\""
+            MACOS_BUNDLE_POST_BUILD
+    )
+    install(SCRIPT ${deploy_script})
 
-
-install(DIRECTORY data/themes/ DESTINATION data/themes/
-        COMPONENT RhythmGame_Runtime)
-
-install(DIRECTORY DESTINATION data/profiles/
-        COMPONENT RhythmGame_Runtime)
-
-install(DIRECTORY DESTINATION data/tables/
-        COMPONENT RhythmGame_Runtime)
-
-qt_generate_deploy_qml_app_script(
-        TARGET RhythmGame_exe
-        OUTPUT_SCRIPT deploy_script
-        NO_UNSUPPORTED_PLATFORM_ERROR
-        DEPLOY_TOOL_OPTIONS "--qmldir \"${CMAKE_SOURCE_DIR}/data/themes/Default\""
-        MACOS_BUNDLE_POST_BUILD
-)
-install(SCRIPT ${deploy_script})
-
-if (LINUX)
-    install(FILES RhythmGame.sh DESTINATION "${CMAKE_INSTALL_BINDIR}" PERMISSIONS OWNER_EXECUTE OWNER_READ OWNER_WRITE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE)
+    if (LINUX)
+        install(FILES RhythmGame.sh DESTINATION "${CMAKE_INSTALL_BINDIR}" PERMISSIONS OWNER_EXECUTE OWNER_READ OWNER_WRITE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE)
+    endif ()
 endif ()
+
+set(DATA_FOLDER_PREFIX "data" CACHE STRING "Prefix for data folder installation")
+
+install(FILES data/avatars/mascot.png DESTINATION ${DATA_FOLDER_PREFIX}/avatars/mascot.png
+        COMPONENT RhythmGame_Runtime)
+
+install(DIRECTORY data/themes/Default DESTINATION ${DATA_FOLDER_PREFIX}/themes/Default
+        COMPONENT RhythmGame_Runtime)
+
+install(DIRECTORY DESTINATION ${DATA_FOLDER_PREFIX}/profiles/
+        COMPONENT RhythmGame_Runtime)
+
+install(DIRECTORY DESTINATION ${DATA_FOLDER_PREFIX}/tables/
+        COMPONENT RhythmGame_Runtime)
+
 
 set(CPACK_PACKAGE_VENDOR "Bobini")
 set(CPACK_PACKAGE_INSTALL_DIRECTORY "RhythmGame")
@@ -46,7 +51,7 @@ if (WIN32)
             COMPONENT RhythmGame_Runtime)
 
     set(CPACK_NSIS_EXTRA_INSTALL_COMMANDS
-        "Exec 'icacls \\\"$INSTDIR/data\\\" /grant *S-1-5-32-545:(OI)(CI)F /T'"
+            "Exec 'icacls \\\"$INSTDIR/data\\\" /grant *S-1-5-32-545:(OI)(CI)F /T'"
     )
     set(CPACK_PACKAGE_EXECUTABLES "RhythmGame" "RhythmGame")
 endif ()
