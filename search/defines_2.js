0ba7527cb175456571455c6985574b7f3763b9dd
Bobini1 <Bobini1@users.noreply.github.com>
2025-09-16 02:43:26 +0000

    deploy: 73d81d31f445c99efe5425344559b79b6940868e

--- search/defines_2.js
+++ search/defines_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['miniaudio_5fimplementation_0',['MINIAUDIO_IMPLEMENTATION',['../MiniaudioImpl_8cpp.html#abf77f70dc931f91a9f09e7b54d8278f5',1,'MiniaudioImpl.cpp']]]
+  ['miniaudio_5fimplementation_0',['MINIAUDIO_IMPLEMENTATION',['../MiniaudioBackend_8cpp.html#abf77f70dc931f91a9f09e7b54d8278f5',1,'MiniaudioBackend.cpp']]]
 ];
--- search/files_b.js
+++ search/files_b.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['main_2ecpp_0',['main.cpp',['../main_8cpp.html',1,'']]],
-  ['miniaudioimpl_2ecpp_1',['MiniaudioImpl.cpp',['../MiniaudioImpl_8cpp.html',1,'']]]
+  ['miniaudiobackend_2ecpp_1',['MiniaudioBackend.cpp',['../MiniaudioBackend_8cpp.html',1,'']]],
+  ['miniaudiobackend_2eh_2',['MiniaudioBackend.h',['../MiniaudioBackend_8h.html',1,'']]]
 ];
