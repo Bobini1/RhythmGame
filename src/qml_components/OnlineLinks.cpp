e5ff93cd55b55f3759e2e8e2cc358aee0231be7a
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2026-03-18 05:40:04 +0100

    Fix chart links

--- src/qml_components/OnlineLinks.cpp
+++ src/qml_components/OnlineLinks.cpp
@@ -18,12 +18,12 @@ OnlineLinks::profile(QString websiteUrl, int profileId)
 QString
 OnlineLinks::chart(QString websiteUrl, QString md5)
 {
-    return websiteUrl + "/chart/" + md5;
+    return websiteUrl + "/charts/" + md5;
 }
 QString
 OnlineLinks::scoresByUserOnChart(QString websiteUrl, int profileId, QString md5)
 {
-    return websiteUrl + "/chart/" + md5 + "/players/" +
+    return websiteUrl + "/charts/" + md5 + "/players/" +
            QString::number(profileId) + "/scores";
 }
 } // namespace qml_components
