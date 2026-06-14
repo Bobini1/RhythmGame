9bb495b6da0bbf6c223323ffde67b90bf6fceeb6
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2026-06-15 00:11:28 +0200

    Fix OpenXR on Linux

---
+++ vcpkgOverlayPorts/openexr/portfile.cmake
@@ -0,0 +1,61 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO AcademySoftwareFoundation/openexr
+    REF "v${VERSION}"
+    SHA512 ec33f1e05346cf761f0fd5740c3894fe58840a800b7df9b44f7ebc3ced420cfc756c580ef898d0cc80898f0e0ae50ae99000731905e146236d81b32f9974b3dd
+    HEAD_REF main
+    PATCHES
+        fix-c23-once-flag.patch
+)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS OPTIONS
+    FEATURES
+        tools   OPENEXR_BUILD_TOOLS
+        tools   OPENEXR_INSTALL_TOOLS
+)
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        ${OPTIONS}
+        -DBUILD_TESTING=OFF
+        -DBUILD_WEBSITE=OFF
+        -DCMAKE_REQUIRE_FIND_PACKAGE_libdeflate=ON
+        -DOPENEXR_BUILD_EXAMPLES=OFF
+        -DOPENEXR_INSTALL_PKG_CONFIG=ON
+    OPTIONS_DEBUG
+        -DOPENEXR_BUILD_TOOLS=OFF
+        -DOPENEXR_INSTALL_TOOLS=OFF
+)
+vcpkg_cmake_install()
+vcpkg_copy_pdbs()
+
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/OpenEXR)
+
+vcpkg_fixup_pkgconfig()
+
+if(OPENEXR_INSTALL_TOOLS)
+    vcpkg_copy_tools(
+        TOOL_NAMES
+            exr2aces
+            # not installed: exrcheck
+            exrenvmap
+            exrheader
+            exrinfo
+            exrmakepreview
+            exrmaketiled
+            exrmanifest
+            exrmetrics
+            exrmultipart
+            exrmultiview
+            exrstdattr
+        AUTO_CLEAN
+    )
+endif()
+
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/debug/share"
+)
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.md")
