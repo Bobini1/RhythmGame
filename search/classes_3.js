4ca54943f6c88a8d8f10d075fc76d6713d20ebd0
Bobini1 <Bobini1@users.noreply.github.com>
2026-06-14 21:48:58 +0000

    deploy: 1f6458e9d347dd65430396375dc9dc6579f49f9c

--- search/classes_3.js
+++ search/classes_3.js
@@ -1,12 +1,14 @@
 var searchData=
 [
-  ['defexranktag_0',['DefExRankTag',['../structcharts_1_1DefExRankTag.html',1,'charts']]],
-  ['deviceid_1',['DeviceId',['../structsounds_1_1AudioEngine_1_1DeviceId.html',1,'sounds::AudioEngine']]],
-  ['difficulty_2',['Difficulty',['../structcharts_1_1Difficulty.html',1,'charts']]],
-  ['difficultytag_3',['DifficultyTag',['../structcharts_1_1DifficultyTag.html',1,'charts']]],
-  ['dst_4',['Dst',['../structlr2skin_1_1runtime_1_1Dst.html',1,'lr2skin::runtime']]],
-  ['dstanalysis_5',['DstAnalysis',['../structlr2skin_1_1runtime_1_1DstAnalysis.html',1,'lr2skin::runtime']]],
-  ['dto_6',['dto',['../structgameplay__logic_1_1BmsGaugeHistory_1_1DTO.html',1,'gameplay_logic::BmsGaugeHistory::DTO'],['../structgameplay__logic_1_1BmsReplayData_1_1DTO.html',1,'gameplay_logic::BmsReplayData::DTO'],['../structgameplay__logic_1_1BmsResult_1_1DTO.html',1,'gameplay_logic::BmsResult::DTO'],['../structgameplay__logic_1_1BmsResultCourse_1_1DTO.html',1,'gameplay_logic::BmsResultCourse::DTO'],['../structgameplay__logic_1_1ChartData_1_1DTO.html',1,'gameplay_logic::ChartData::DTO']]],
-  ['dxaimageprovider_7',['DxaImageProvider',['../classresource__managers_1_1DxaImageProvider.html',1,'resource_managers']]],
-  ['dxarchivesegment_8',['DXArchiveSegment',['../structsupport_1_1DXArchiveSegment.html',1,'support']]]
+  ['ddshandler_0',['DdsHandler',['../classDdsHandler.html',1,'']]],
+  ['ddsplugin_1',['DdsPlugin',['../classDdsPlugin.html',1,'']]],
+  ['defexranktag_2',['DefExRankTag',['../structcharts_1_1DefExRankTag.html',1,'charts']]],
+  ['deviceid_3',['DeviceId',['../structsounds_1_1AudioEngine_1_1DeviceId.html',1,'sounds::AudioEngine']]],
+  ['difficulty_4',['Difficulty',['../structcharts_1_1Difficulty.html',1,'charts']]],
+  ['difficultytag_5',['DifficultyTag',['../structcharts_1_1DifficultyTag.html',1,'charts']]],
+  ['dst_6',['Dst',['../structlr2skin_1_1runtime_1_1Dst.html',1,'lr2skin::runtime']]],
+  ['dstanalysis_7',['DstAnalysis',['../structlr2skin_1_1runtime_1_1DstAnalysis.html',1,'lr2skin::runtime']]],
+  ['dto_8',['dto',['../structgameplay__logic_1_1BmsGaugeHistory_1_1DTO.html',1,'gameplay_logic::BmsGaugeHistory::DTO'],['../structgameplay__logic_1_1BmsReplayData_1_1DTO.html',1,'gameplay_logic::BmsReplayData::DTO'],['../structgameplay__logic_1_1BmsResult_1_1DTO.html',1,'gameplay_logic::BmsResult::DTO'],['../structgameplay__logic_1_1BmsResultCourse_1_1DTO.html',1,'gameplay_logic::BmsResultCourse::DTO'],['../structgameplay__logic_1_1ChartData_1_1DTO.html',1,'gameplay_logic::ChartData::DTO']]],
+  ['dxaimageprovider_9',['DxaImageProvider',['../classresource__managers_1_1DxaImageProvider.html',1,'resource_managers']]],
+  ['dxarchivesegment_10',['DXArchiveSegment',['../structsupport_1_1DXArchiveSegment.html',1,'support']]]
 ];
--- search/files_3.js
+++ search/files_3.js
@@ -1,12 +1,16 @@
 var searchData=
 [
-  ['definedb_2ecpp_0',['DefineDb.cpp',['../DefineDb_8cpp.html',1,'']]],
-  ['definedb_2eh_1',['DefineDb.h',['../DefineDb_8h.html',1,'']]],
-  ['dev_5fengine_2emd_2',['DEV_ENGINE.md',['../DEV__ENGINE_8md.html',1,'']]],
-  ['dev_5flang_2emd_3',['DEV_LANG.md',['../DEV__LANG_8md.html',1,'']]],
-  ['dev_5ftheme_2emd_4',['DEV_THEME.md',['../DEV__THEME_8md.html',1,'']]],
-  ['dxa_2ecpp_5',['dxa.cpp',['../dxa_8cpp.html',1,'']]],
-  ['dxa_2eh_6',['dxa.h',['../dxa_8h.html',1,'']]],
-  ['dxaimageprovider_2ecpp_7',['DxaImageProvider.cpp',['../DxaImageProvider_8cpp.html',1,'']]],
-  ['dxaimageprovider_2eh_8',['DxaImageProvider.h',['../DxaImageProvider_8h.html',1,'']]]
+  ['ddshandler_2ecpp_0',['DdsHandler.cpp',['../DdsHandler_8cpp.html',1,'']]],
+  ['ddshandler_2eh_1',['DdsHandler.h',['../DdsHandler_8h.html',1,'']]],
+  ['ddsplugin_2ecpp_2',['DdsPlugin.cpp',['../DdsPlugin_8cpp.html',1,'']]],
+  ['ddsplugin_2eh_3',['DdsPlugin.h',['../DdsPlugin_8h.html',1,'']]],
+  ['definedb_2ecpp_4',['DefineDb.cpp',['../DefineDb_8cpp.html',1,'']]],
+  ['definedb_2eh_5',['DefineDb.h',['../DefineDb_8h.html',1,'']]],
+  ['dev_5fengine_2emd_6',['DEV_ENGINE.md',['../DEV__ENGINE_8md.html',1,'']]],
+  ['dev_5flang_2emd_7',['DEV_LANG.md',['../DEV__LANG_8md.html',1,'']]],
+  ['dev_5ftheme_2emd_8',['DEV_THEME.md',['../DEV__THEME_8md.html',1,'']]],
+  ['dxa_2ecpp_9',['dxa.cpp',['../dxa_8cpp.html',1,'']]],
+  ['dxa_2eh_10',['dxa.h',['../dxa_8h.html',1,'']]],
+  ['dxaimageprovider_2ecpp_11',['DxaImageProvider.cpp',['../DxaImageProvider_8cpp.html',1,'']]],
+  ['dxaimageprovider_2eh_12',['DxaImageProvider.h',['../DxaImageProvider_8h.html',1,'']]]
 ];
