fdfcb91121d21c5470ccb0c9b140fc25e4e0f613
Bobini1 <Bobini1@users.noreply.github.com>
2026-08-09 13:58:47 +0000

    deploy: e80507b974b6d679d6663c1867912745911e231a

--- search/typedefs_11.js
+++ search/typedefs_11.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['wav_5ft_0',['wav_t',['../namespacecharts.html#a9829ed4fc2d4d39ee97d1b460638c72f',1,'charts']]]
+  ['wantscontents_0',['WantsContents',['../classresource__managers_1_1SongAssetStore.html#a366901fe76ca8c8227bad79265eed5ec',1,'resource_managers::SongAssetStore']]],
+  ['wav_5ft_1',['wav_t',['../namespacecharts.html#a9829ed4fc2d4d39ee97d1b460638c72f',1,'charts']]]
 ];
--- search/typedefs_4.js
+++ search/typedefs_4.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['encodearenabinaryresult_0',['EncodeArenaBinaryResult',['../namespacearena.html#adefa2d8b2e5fdcb61bb372e76d797d3a',1,'arena']]],
-  ['encodeclientresult_1',['EncodeClientResult',['../namespacearena.html#a5594253d1a83ccaa791fbf7b18d3fb97',1,'arena']]]
+  ['encodeclientresult_1',['EncodeClientResult',['../namespacearena.html#a5594253d1a83ccaa791fbf7b18d3fb97',1,'arena']]],
+  ['entryvisitor_2',['EntryVisitor',['../classresource__managers_1_1SongAssetStore.html#a54f4d33ba3b5ab4d9906289d753c466a',1,'resource_managers::SongAssetStore']]]
 ];
